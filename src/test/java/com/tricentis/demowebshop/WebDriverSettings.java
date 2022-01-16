9a489fcfba7de0c17e1e8c53d5c5653b17ca6d44
Olia Orel <olia.orel13@gmail.com>
2022-01-16 22:19:20 +0200

    Update selenium tests

--- src/test/java/com/tricentis/demowebshop/WebDriverSettings.java
+++ src/test/java/com/tricentis/demowebshop/WebDriverSettings.java
@@ -2,8 +2,6 @@ package com.tricentis.demowebshop;
 
 import org.junit.After;
 import org.junit.Before;
-import org.openqa.selenium.By;
-import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 
 public class WebDriverSettings {
@@ -19,17 +17,4 @@ public class WebDriverSettings {
     public void close() {
         driver.quit();
     }
-
-//    public static WebElement findElement(By by)
-//    {
-//        return driver.findElement(by);
-//    }
-
-//    public static By ComputerCategory() {
-//        return By.xpath("//div[@class='block-category-navigation']//a[@Text='Computers']");
-//    }
-
-//    public static void clickComputerCategory() {
-//        findElement(ComputerCategory());
-//    }
 }
