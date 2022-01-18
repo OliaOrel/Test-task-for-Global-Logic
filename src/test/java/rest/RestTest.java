1072a3e2b02500eb620ef0cc260612064c55558e
Olia Orel <olia.orel13@gmail.com>
2022-01-18 15:23:26 +0200

    Update api tests

--- src/test/java/rest/RestTest.java
+++ src/test/java/rest/RestTest.java
@@ -1,10 +1,8 @@
 package rest;
 
 import io.restassured.RestAssured;
-import io.restassured.builder.RequestSpecBuilder;
 import io.restassured.http.ContentType;
 import io.restassured.response.Response;
-import io.restassured.specification.RequestSpecification;
 import org.json.simple.JSONArray;
 import org.json.simple.JSONObject;
 import org.json.simple.parser.JSONParser;
@@ -14,6 +12,9 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import static io.restassured.RestAssured.given;
 
@@ -25,34 +26,45 @@ public class RestTest {
     public static void setUp() throws ParseException {
         RestAssured.baseURI ="https://restful-booker.herokuapp.com";
 
-        String apiBody = "{\"username\":\"admin\",\"password\":\"password123\"}";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("username", "admin");
+        requestParams.put("password", "password123");
 
-        Response response = given()
+        String response = given()
                 .contentType(ContentType.JSON)
-                .body(apiBody)
+                .body(requestParams.toJSONString())
                 .when()
                 .post("/auth")
                 .then()
                 .statusCode(200)
                 .extract()
-                .response();
+                .response()
+                .getBody()
+                .asString();
 
         JSONParser parser = new JSONParser();
-        JSONObject body = (JSONObject) parser.parse(response.body().asString());
+        JSONObject body = (JSONObject) parser.parse(response);
         token = (String) body.get("token");
-
-//        System.out.println(token);
     }
 
     @BeforeEach
     public void createBooking() throws ParseException {
-        System.out.println("BeforeEach");
+        JSONObject bookingdates = new JSONObject();
+        bookingdates.put("checkin", "2018-01-01");
+        bookingdates.put("checkout", "2019-01-01");
 
-        String apiBody = "{\"firstname\" : \"Jim\",\"lastname\" : \"Brown\",\"totalprice\" : 111,\"depositpaid\" : true,\"bookingdates\" : {\"checkin\" : \"2018-01-01\",\"checkout\" : \"2019-01-01\"}, \"additionalneeds\" : \"Breakfast\"}";
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("firstname", "Jim");
+        requestParams.put("lastname", "Brown");
+        requestParams.put("totalprice", 111);
+        requestParams.put("depositpaid", true);
+        requestParams.put("bookingdates", bookingdates);
+        requestParams.put("additionalneeds", "Breakfast");
+
         Response response = given()
                 .header("Content-type", "application/json")
                 .header("Accept", "application/json")
-                .body(apiBody)
+                .body(requestParams.toJSONString())
                 .when()
                 .post("booking")
                 .then()
@@ -62,85 +74,93 @@ public class RestTest {
 
         JSONParser parser = new JSONParser();
         JSONObject body = (JSONObject) parser.parse(response.body().asString());
-
         bookingid = (long) body.get("bookingid");
-        System.out.println(bookingid);
     }
 
-    @Test
-    public void getDetails() throws ParseException {
-        System.out.println(token);
-        System.out.println(bookingid);
-
-        Response response = given()
+    public JSONObject getDetails() throws ParseException {
+        String response = given()
                 .header("Accept", "application/json")
                 .when()
                 .get("booking/" + bookingid)
                 .then()
                 .statusCode(200)
                 .extract()
-                .response();
+                .response()
+                .getBody()
+                .asString();
 
         JSONParser parser = new JSONParser();
-        JSONObject body = (JSONObject) parser.parse(response.body().asString());
-        String bodyText = body.toString();
-
-//        System.out.println(bodyText);
+        JSONObject body = (JSONObject) parser.parse(response);
+        return body;
     }
 
     @Test
-    public void updateDetails() throws ParseException {
-//        System.out.println(bookingid);
-        String apiBody = "{\"firstname\" : \"Lolo\",\"totalprice\" : 222}";
+    public void getDetailsTest() throws ParseException {
+        JSONObject details = getDetails();
 
-        Response response = given()
-                .header("Content-type", "application/json")
+        Assert.assertEquals(details.get("firstname"), "Jim");
+        Assert.assertEquals(details.get("lastname"), "Brown");
+        Assert.assertEquals((long) details.get("totalprice"), 111);
+        Assert.assertEquals(details.get("depositpaid"), true);
+        Assert.assertEquals(((JSONObject) details.get("bookingdates")).get("checkin"), "2018-01-01");
+        Assert.assertEquals(((JSONObject) details.get("bookingdates")).get("checkout"), "2019-01-01");
+        Assert.assertEquals(details.get("additionalneeds"), "Breakfast");
+    }
+
+    @Test
+    public void updateDetailsTest() throws ParseException {
+        JSONObject requestParams = new JSONObject();
+        requestParams.put("firstname", "Lolo");
+        requestParams.put("totalprice", 222);
+
+        given().header("Content-type", "application/json")
                 .header("Accept", "application/json")
                 .header("Cookie", "token=" + token)
-                .body(apiBody)
+                .body(requestParams.toJSONString())
                 .when()
                 .patch("booking/" + bookingid)
                 .then()
-                .statusCode(200)
-                .extract()
-                .response();
+                .statusCode(200);
 
-        JSONParser parser = new JSONParser();
-        JSONObject body = (JSONObject) parser.parse(response.body().asString());
-        String bodyText = body.toString();
+        JSONObject details = getDetails();
 
-//        System.out.println(bodyText);
+        Assert.assertEquals(details.get("firstname"), "Lolo");
+        Assert.assertEquals(details.get("lastname"), "Brown");
+        Assert.assertEquals((long) details.get("totalprice"), 222);
+        Assert.assertEquals(details.get("depositpaid"), true);
+        Assert.assertEquals(((JSONObject) details.get("bookingdates")).get("checkin"), "2018-01-01");
+        Assert.assertEquals(((JSONObject) details.get("bookingdates")).get("checkout"), "2019-01-01");
+        Assert.assertEquals(details.get("additionalneeds"), "Breakfast");
     }
 
     @Test
-    public void getAllBookings() throws ParseException {
-        Response response = given()
+    public void getBookingIDsTest() throws ParseException {
+        String response = given()
                 .when()
-                .get("booking/" + "")
+                .get("booking/")
                 .then()
                 .statusCode(200)
                 .extract()
-                .response();
+                .response()
+                .getBody()
+                .asString();
 
         JSONParser parser = new JSONParser();
-        JSONArray body = (JSONArray) parser.parse(response.body().asString());
-        String bodyText = body.toString();
+        JSONArray body = (JSONArray) parser.parse(response);
 
-        System.out.println(bodyText);
+        List<Long> arr = new ArrayList<>();
+        body.forEach(el -> arr.add((Long) ((JSONObject) el).get("bookingid")));
+
+        Assert.assertTrue(arr.contains(bookingid));
     }
 
     @AfterEach
     public void deleteBooking() {
-        Response response = given()
-                .header("Content-type", "application/json")
+        given().header("Content-type", "application/json")
                 .header("Cookie", "token=" + token)
                 .when()
                 .delete("booking/" + bookingid)
                 .then()
-                .statusCode(201)
-                .extract()
-                .response();
-
-        System.out.println("deleteBooking");
+                .statusCode(201);
     }
 }
